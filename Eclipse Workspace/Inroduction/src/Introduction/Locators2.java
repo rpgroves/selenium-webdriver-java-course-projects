711aeca601bb6fbf5adb0ef1a135a65fe51ef86f
Rylee Groves <43833545+rpgroves@users.noreply.github.com>
2025-08-10 21:07:09 -0400

    Removed module-info in order for TestNG to function

--- Eclipse Workspace/Inroduction/src/Introduction/Locators2.java
+++ Eclipse Workspace/Inroduction/src/Introduction/Locators2.java
@@ -1,5 +1,7 @@
 package Introduction;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,10 +14,16 @@ public class Locators2 {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/locatorspractice");
 		driver.findElement(By.id("inputUsername")).sendKeys("Rylee Groves");
-		driver.findElement(By.name("inputPassword")).sendKeys("secretPass123!");
+		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
 		driver.findElement(By.className("signInBtn")).click();
-		System.out.println(driver.findElement(By.tagName("p")).getText());
 		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			System.out.println("Interrupted Exception");
+		}
+		
+		System.out.println(driver.findElement(By.tagName("p")).getText());
 		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
 	}
 }
