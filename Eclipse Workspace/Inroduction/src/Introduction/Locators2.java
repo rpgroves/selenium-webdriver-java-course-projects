cb55d298ee749fbea0d05ba9310baf32dcf8bb66
Rylee Groves <43833545+rpgroves@users.noreply.github.com>
2025-08-12 21:51:15 -0400

    Added string parsing to Locators2

--- Eclipse Workspace/Inroduction/src/Introduction/Locators2.java
+++ Eclipse Workspace/Inroduction/src/Introduction/Locators2.java
@@ -5,19 +5,26 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class Locators2 {
 
 	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
 		
 		String name = "Rylee Groves";
+		String password = "";
+		password = getPassword(driver);
+		System.out.println(password);
 		
-		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/locatorspractice");
 		driver.findElement(By.id("inputUsername")).sendKeys(name);
-		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.name("inputPassword")).sendKeys(password);
 		driver.findElement(By.className("signInBtn")).click();
 		
 		try {
@@ -29,5 +36,23 @@ public class Locators2 {
 		System.out.println(driver.findElement(By.tagName("p")).getText());
 		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
 		Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello " + name + ",");
+		
+		driver.quit();
+	}
+	
+	public static String getPassword(WebDriver driver)
+	{
+		driver.get("https://rahulshettyacademy.com/locatorspractice");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			System.out.println("Interrupted Exception");
+		}
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+		return passwordText.split("'")[1].split("'")[0];
 	}
 }
