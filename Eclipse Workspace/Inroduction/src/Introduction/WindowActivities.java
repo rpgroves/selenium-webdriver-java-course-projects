401457ba08627d624ee4e4e9be282fdc764f46d7
Rylee Groves <43833545+rpgroves@users.noreply.github.com>
2025-08-15 22:20:19 -0400

    Added windowActivities File for testing window selenium functions

--- Eclipse Workspace/Inroduction/src/Introduction/WindowActivities.java
+++ Eclipse Workspace/Inroduction/src/Introduction/WindowActivities.java
@@ -1,5 +1,46 @@
 package Introduction;
 
-public class WindowActivities {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class WindowActivities {
+	public static void main(String[] args)
+	{
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		//waits for all components to fully load on a page
+		driver.get("https://google.com");
+		
+		//does not wait for all components
+		driver.navigate().to("https://rahulshettyacademy.com");
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			System.out.println("Interrupted Exception");
+		}
+		
+		//goes back a page
+		driver.navigate().back();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			System.out.println("Interrupted Exception");
+		}
+		
+		//goes forward a page
+		driver.navigate().forward();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			System.out.println("Interrupted Exception");
+		}
+		
+		driver.quit();
+	}
 }
