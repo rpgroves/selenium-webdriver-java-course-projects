482239658f30cce5d5c33dffd188641ef7c5ed86
Rylee Groves <43833545+rpgroves@users.noreply.github.com>
2025-08-17 23:46:54 -0400

    Added Checkbox.java
    Tested finding and selecting checkboxes

---
+++ Eclipse Workspace/Inroduction/src/Introduction/CheckBox.java
@@ -0,0 +1,24 @@
+package Introduction;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CheckBox {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise");
+		
+		driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).click();
+		System.out.println(driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).isSelected());
+
+		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+		
+	}
+
+}
