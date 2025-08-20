37ce15e641fc7cc6b336348c5505b888399781dd
Rylee Groves <43833545+rpgroves@users.noreply.github.com>
2025-08-20 00:06:35 -0400

    Completed UpdatedDropdown.java
    Test uses the senior discount checkbox and changed reservation to 5 adults with assertions to test that inputs went through

--- Eclipse Workspace/Inroduction/src/Introduction/UpdatedDropdown.java
+++ Eclipse Workspace/Inroduction/src/Introduction/UpdatedDropdown.java
@@ -17,6 +17,25 @@ public class UpdatedDropdown {
 		Assert.assertFalse(seniorCitizenDiscount.isSelected());
 		seniorCitizenDiscount.click();
 		Assert.assertTrue(seniorCitizenDiscount.isSelected());
+		
+		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "1 Adult");
+		driver.findElement(By.id("divpaxinfo")).click();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			System.out.println("Interrupted Exception");
+		}
+		
+		for(int i = 1; i < 5; i++)
+		{
+			driver.findElement(By.id("hrefIncAdt")).click();
+		}
+		
+		driver.findElement(By.id("btnclosepaxoption")).click();
+		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
+		
+		driver.quit();
 
 	}
 	
